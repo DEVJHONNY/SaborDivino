bd5780f2535074df517197636260be0638ad8dbe
Lucas dos Anjos A. Rocha <104236063+DEVJHONNY@users.noreply.github.com>
2025-02-26 13:57:53 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -2,22 +2,21 @@
 const PRODUTOS_PADRAO = {
     trufas: [
         { id: 1, nome: 'Trufa de Brigadeiro', preco: 4.50, estoque: 0 },
-        { id: 2, nome: 'Trufa de Ninho', preco: 4.50, estoque: 0},
+        { id: 2, nome: 'Trufa de Ninho', preco: 4.50, estoque: 0 },
         { id: 3, nome: 'Trufa de Ovomaltine', preco: 4.50, estoque: 0 },
         { id: 4, nome: 'Trufa de Ninho', preco: 4.50, estoque: 0 },
         { id: 5, nome: 'Trufa de Coco', preco: 4.50, estoque: 2 },
         { id: 6, nome: 'Trufa de Amendoim', preco: 4.50, estoque: 4 },
         { id: 7, nome: 'Trufa de Ferreiro', preco: 4.50, estoque: 3 },
-        { id: 8, nome: 'Trufa de Castanha', preco: 4.50, estoque: 3 },
-        
+        { id: 8, nome: 'Trufa de Castanha', preco: 4.50, estoque: 3 }
     ],
     mousses: [
-        { id: 6, nome: 'Mousse de Limão', preco: 3.50, estoque: 3 },
-        { id: 7, nome: 'Mousse de Maracujá', preco: 3.50, estoque: 3 }
+        { id: 9, nome: 'Mousse de Limão', preco: 3.50, estoque: 3 },
+        { id: 10, nome: 'Mousse de Maracujá', preco: 3.50, estoque: 3 }
     ],
     empadas: [
-        { id: 8, nome: 'Empada de Frango', preco: 4.50, estoque: 0 },
-        { id: 9, nome: 'Empada de Calabresa', preco: 4.50, estoque: 0 }
+        { id: 11, nome: 'Empada de Frango', preco: 4.50, estoque: 0 },
+        { id: 12, nome: 'Empada de Calabresa', preco: 4.50, estoque: 0 }
     ]
 };
 
